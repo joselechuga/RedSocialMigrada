4b4d454ce697d90c01196ee57b61115f5edf136f
joselechuga <jos.medinav@duocuc.cl>
2022-06-24 22:56:14 -0400

    conect db

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class usuarios(models.Model):
+    usuario = models.CharField(max_length=50,verbose_name="nick usuario")
+    nombre = models.CharField(max_length=50,verbose_name="nombre usuario")
+    password = models.CharField(max_length=50,verbose_name="contraseña")
+    password2 = models.CharField(max_length=50,verbose_name="confirmacion contraseña")
+    correo = models.EmailField(max_length=50,verbose_name="correo")
+
+
+    def __str__(self):
+        return self.usuario
+
